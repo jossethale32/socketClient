1eac8fa095b87be1d04c31f5670ace07939ebc8c
jossethale32 <jabautista1@uth.hn>
2023-11-26 16:42:22 -0600

    Agregar archivos de proyecto.

---
+++ sockettest/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace socketclient
+{
+    internal class Client
+    {
+        IPHostEntry host;
+        IPAddress remoteAddress;
+        IPEndPoint remoteEndPoint;
+
+        Socket s_cli;
+
+        public Client(string ip, int port)
+        {
+            host = Dns.GetHostByName(ip);
+            remoteAddress = host.AddressList[0];
+            remoteEndPoint = new IPEndPoint(remoteAddress, port);
+
+            s_cli = new Socket(remoteAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            //s_cli.Connect(remoteEndPoint);
+        }
+        public void Start()
+        {
+            s_cli.Connect(remoteEndPoint);
+
+        }
+        public void Send(string msj)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(msj);
+            s_cli.Send(bytes);
+            Console.WriteLine("Mensaje Enviado desde cliente");
+        }
+    }
+}
---
+++ sockettest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace socketclient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client s = new Client("127.0.0.1", 3367);
+            s.Start();
+            s.Send("Hola soy el cliente");
+        }
+    }
+
+}
